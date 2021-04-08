8ee1f4a135942e6c95a0b43fd3b9b6d052cbb990
CarlosSantiago Nevett <csnevett@gmail.com>
2021-04-08 14:39:23 -0400

    wip

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 
 const server = express();
 
 server.use(express.json());
+server.use(express.static(path.join(__dirname, 'client/build'))); //building absolute path regardless of machine
 
 if (process.env.NODE_ENV === 'development') {
 	//on Heroku, an env variable is called "NODE_ENV" -> 'production'
@@ -11,8 +13,13 @@ if (process.env.NODE_ENV === 'development') {
 	server.use(cors());
 }
 
-server.use('*', (req, res) => {
-	res.send('<h1>success</h1>');
+server.get('/api/hello', (req, res) => {
+	res.json({ message: 'hello' });
+});
+
+//CATCH ALL ENDPOINT THAT SENDS BACK index.html
+server.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 4000;
